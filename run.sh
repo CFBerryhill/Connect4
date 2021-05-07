ed8a38e61b5c0f2f39c1a079cfbc155bf414b4b5
Casey Ford <fordcb@lafayette.edu>
2021-05-07 10:55:41 -0500

    preparing to run on server

---
+++ run.sh
@@ -0,0 +1,9 @@
+java \
+-Dfile.encoding=UTF-8 \
+-classpath \
+/home/fordcb/Connect4/target/scala-2.13/classes:\
+/home/fordcb/.cache/coursier/v1/https/repo1.maven.org/maven2/org/scala-lang/scala-library/2.13.4/scala-library-2.13.4.jar:\
+/home/fordcb/.cache/coursier/v1/https/repo1.maven.org/maven2/org/scala-lang/scala-reflect/2.13.4/scala-reflect-2.13.4.jar:\
+/home/fordcb/.cache/coursier/v1/https/repo1.maven.org/maven2/org/scala-lang/scala-library/2.13.4/scala-library-2.13.4-sources.jar:\
+/home/fordcb/.cache/coursier/v1/https/repo1.maven.org/maven2/org/scala-lang/scala-reflect/2.13.4/scala-reflect-2.13.4-sources.jar \
+Engine.Controller
